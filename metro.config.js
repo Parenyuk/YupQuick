4ac6850e58134d5c7f1f910ad466af9efba5b574
Parenyuk <ParenVic@i.ua>
2026-04-02 11:16:10 +0300

    YUQ-001, added icons

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
 config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
 
-module.exports = withNativeWind(config, { input: './global.css' });
+const nativeWindConfig = withNativeWind(config, { input: './global.css' });
+
+nativeWindConfig.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
+module.exports = nativeWindConfig;
--- src/components/units/MenuBar/index.tsx
+++ src/components/units/MenuBar/index.tsx
@@ -2,9 +2,10 @@ import {View,  Text} from "react-native";
 import HomeIcon from '@/assets/svg/home.svg';
 
 const MenuBar = () => {
-    return (<View>
-        <Text>q23</Text>
+    return (<View className='bg-red-500 w-full h-full'>
+        <Text className='bg-red-500 text-3xl'>q23333 111 </Text>
         <HomeIcon width={40} height={40} />
+        {/*<MenuIcon width={40} height={40 />*/}
     </View>)
 };
 
