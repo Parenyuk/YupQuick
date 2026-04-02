942e3d72c62e4d1424b102aa5f7374e9f1153374
Parenyuk <ParenVic@i.ua>
2026-04-02 20:59:37 +0300

    YUQ-001, added BestSellerBlock

--- app/_components/BestSellerBlock/index.tsx
+++ app/_components/BestSellerBlock/index.tsx
@@ -1,26 +1,30 @@
-import {Image, ImageSourcePropType, Text, View} from "react-native";
+import { Image, ImageSourcePropType, View } from "react-native";
 import SushiImg from "@/assets/png/sushi.png";
 import RiceImg from "@/assets/png/rice.png";
 import DessertOneImg from "@/assets/png/dessert-1.png";
 import DessertTwoImg from "@/assets/png/dessert-2.png";
 
 const BestSellerBlock = () => {
-
-    const bestSellerArr: { id: number; icon: ImageSourcePropType; }[] = [
+    const bestSellerArr: { id: number; icon: ImageSourcePropType }[] = [
         { id: 0, icon: SushiImg },
         { id: 1, icon: RiceImg },
         { id: 2, icon: DessertOneImg },
         { id: 3, icon: DessertTwoImg },
     ];
 
-
-    return (<View className='flex-row items-start justify-between'>
-        {bestSellerArr.map(item => (
-            <View className='gap-1' key={item.id}>
-                    <Image source={item.icon} className='w-20 h-27' resizeMode='contain' />
-            </View>
-        ))}
-    </View>)
+    return (
+        <View className="flex-row items-start justify-between gap-4 self-start mt-4">
+            {bestSellerArr.map(item => (
+                <View key={item.id}>
+                    <Image
+                        source={item.icon}
+                        className="w-20 h-28"
+                        resizeMode="contain"
+                    />
+                </View>
+            ))}
+        </View>
+    );
 };
 
 export default BestSellerBlock;
